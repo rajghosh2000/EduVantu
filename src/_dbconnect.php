4049bb30e4c82e66b809968ce2f8a52ed156fd5e
Rajdeep Ghosh <rikta_raj@rediffmail.com>
2021-06-26 17:51:24 +0530

    ...

--- src/_dbconnect.php
+++ src/_dbconnect.php
@@ -1,9 +1,17 @@
 <?php
         //Connecting to database
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "eclass";
+
+        //for local use
+        // $servername = "localhost";
+        // $username = "root";
+        // $password = "";
+        // $database = "eclass";
+
+        //Remote Database Connection
+        $servername = "remotemysql.com";
+        $username = "9GENfndcjA";
+        $password = "o8PK6IOXip";
+        $database = "9GENfndcjA";
 
         $con = mysqli_connect($servername,$username,$password,$database);
 ?>
